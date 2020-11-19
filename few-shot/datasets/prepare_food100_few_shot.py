f0a8af754d79b10ab94a0a0242ba3ecf01e11f04
Weiyao Wang <wangweiyao13@gmail.com>
2020-11-18 22:43:08 -0800

    rename

---
+++ few-shot/datasets/prepare_food100_few_shot.py
@@ -0,0 +1,92 @@
+import numpy as np
+from fvcore.common.file_io import PathManager
+
+import argparse
+import copy
+import os
+import random
+import xml.etree.ElementTree as ET
+
+FOOD_CLASSES = []
+
+dataset_disk = '/home/weiyao/data/'
+uecfood100_path = dataset_disk + 'FOOD100_WW'
+category = 'category.txt'
+
+# Put first column (id) and second column (name) from category.txt into two lists
+category_ids = []
+category_names = []
+with open(uecfood100_path + '/' + category, 'r') as category_list:
+    for i, line in enumerate(category_list):
+        if i > 0:
+            line = line.rstrip('\n')  # delete \n in the end of th
+            # e line
+            line = line.split('\t')
+            FOOD_CLASSES.append(line[1])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seeds", type=int, nargs="+", default=[1, 4],
+                        help="Range of seeds")
+    args = parser.parse_args()
+    return args
+
+
+def generate_seeds(args):
+    data = []
+    data_per_cat = {c: [] for c in FOOD_CLASSES}
+
+    data_file = '/home/weiyao/data/FOOD100_WW/trainval.txt'
+    with PathManager.open(data_file) as f:
+        fileids = np.loadtxt(f, dtype=np.str).tolist()
+    data.extend(fileids)
+
+    for fileid in data:
+        dirname = os.path.join("/home/weiyao/data", "FOOD100_WW")
+        anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
+        try:
+            tree = ET.parse(anno_file)
+        except:
+            continue
+        clses = []
+        for obj in tree.findall("object"):
+            cls = obj.find("name").text
+            clses.append(cls)
+        for cls in set(clses):
+            data_per_cat[cls].append(anno_file)
+
+    result = {cls: {} for cls in data_per_cat.keys()}
+    shots = [1, 2, 3, 5, 10]
+    for i in range(args.seeds[0], args.seeds[1]):
+        random.seed(i)
+        for c in data_per_cat.keys():
+            c_data = []
+            for j, shot in enumerate(shots):
+                diff_shot = shots[j] - shots[j-1] if j != 0 else 1
+                shots_c = random.sample(data_per_cat[c], diff_shot)
+                num_objs = 0
+                for s in shots_c:
+                    if s not in c_data:
+                        tree = ET.parse(s)
+                        file = tree.find("filename").text
+                        name = uecfood100_path + '/Images/{}'.format(file)
+                        c_data.append(name)
+                        for obj in tree.findall("object"):
+                            if obj.find("name").text == c:
+                                num_objs += 1
+                        if num_objs >= diff_shot:
+                            break
+                result[c][shot] = copy.deepcopy(c_data)
+        save_path = 'datasets/food100split/seed{}'.format(i)
+        os.makedirs(save_path, exist_ok=True)
+        for c in result.keys():
+            for shot in result[c].keys():
+                filename = 'box_{}shot_{}_train.txt'.format(shot, c)
+                with open(os.path.join(save_path, filename), 'w') as fp:
+                    fp.write('\n'.join(result[c][shot])+'\n')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    generate_seeds(args)
